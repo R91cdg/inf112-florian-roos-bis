e05cd548cb2ac5d31c945533d188570beeedc1c9
Florian Roos <florian.roos19@gmail.com>
2025-11-18 18:00:06 +0100

    init

---
+++ src/main/java/fr/tp/slr201/projects/robotsim/service/SimulationApplication.java
@@ -0,0 +1,13 @@
+package fr.tp.slr201.projects.robotsim.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulationApplication.class, args);
+	}
+
+}
